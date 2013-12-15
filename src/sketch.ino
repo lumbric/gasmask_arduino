cebb1c28db1968bdf9ad2551b5bddcad0e705bfa
lumbric (antares) <lumbric@gmail.com>
2013-12-15 12:20:13 +0100

    add some more constants to config

--- src/sketch.ino
+++ src/sketch.ino
@@ -7,24 +7,26 @@
 #define SERVO_PIN     9
 #define SENSOR_TRIG   7
 #define SENSOR_ECHO   7
-#define MAX_DISTANCE  200
 
 
-
-const int min_pos           = 0;     // start point of servo in degree (between 0 and 180)
-const int max_pos           = 75;    // end point of servo in degree (between 0 and 180, must be larger than min_pos)
-const int delay_servo       = 1;     // increase value to make servo slower
-const int delay_time        = 50;    // time in milliseconds between 2 nuckels
-const long nuckle_dist_min  = 150;   // nuckels if distance is between min..
-const long nuckle_dist_max  = 400;   // ... and max distance  (if distance > max we assume nobody in museum)
-//long wait_between_min = 10
-
+/***** Config ******/
+// one nuckle is a servo movement in and out again
+const int min_pos           = 0;         // start point of servo in degree (between 0 and 180)
+const int max_pos           = 75;        // end point of servo in degree (between 0 and 180, must be larger than min_pos)
+const int delay_servo       = 1;         // increase value to make servo slower
+const int delay_time        = 50;        // time in milliseconds between 2 nuckels
+const long nuckle_dist      = 150;       // nuckels if distance is nobody is closer than nuckle_dist
+long wait_between_min       = 1 * 1000;  // time between to wait between two nuckle sets in milliseconds
+long wait_between_max       = 10 * 1000; // time between to wait between two nuckle sets in milliseconds
+int number_nuckles_min      = 2;         // nummber of nuckles in one nuckle set
+int number_nuckles_max      = 4;         // nummber of nuckles in one nuckle set
+/***** Config ******/
 
 #define DEBUG 1   // set this to 0 to turn off debug output
 
 Servo servo;
 // see also: http://code.google.com/p/arduino-new-ping/wiki/NewPing_Single_Pin_Sketch
-NewPing sonar(SENSOR_TRIG, SENSOR_TRIG, MAX_DISTANCE);
+NewPing sonar(SENSOR_TRIG, SENSOR_TRIG, nuckle_dist);
 void nuckel();
 long get_distance();
 long last_distance = 0;
@@ -42,14 +44,14 @@ void setup() {
 
 
 void loop() {
-    int nuckel_times = (int) random(2, 5);
+    int nuckel_times = (int) random(number_nuckles_min, number_nuckles_max + 1);
     for (int i = 0; i < nuckel_times; i++) {
         if (delay_until(delay_time) != 0) {
             return;
         }
         nuckel();
     }
-    int wait_time = (int) random(1000, 10000);
+    int wait_time = (int) random(wait_between_min, wait_between_max);
     delay_until(wait_time);
 }
 
@@ -61,8 +63,7 @@ void loop() {
 int delay_until(unsigned long delay_time) {
     unsigned long start = millis();
     while (millis() - start < delay_time){
-        long distance = get_distance();
-        if (! (nuckle_dist_min < distance && distance < nuckle_dist_max ) ) {
+        if (nuckle_dist > get_distance()) {
             return -1;
         }
     }
@@ -88,7 +89,6 @@ void nuckel() {
 
 
 long get_distance() {
-    delay(50);
     if (millis() - last_measured < 50)
         return last_distance;
 
@@ -98,8 +98,9 @@ long get_distance() {
     Serial.print("Distance: ");
     Serial.println(last_distance);
 
+    // 0 means max distance exceeded
     if (last_distance == 0)
-        return MAX_DISTANCE;
+        return nuckle_dist;
     // Convert ping time to distance and print result (0 = outside set distance range, no ping echo)
     return last_distance;
 }
